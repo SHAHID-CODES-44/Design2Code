58b36650f6fa118e899262e37b5f71e7519804b8
shahid-codes-44 <shahidfirozaba@gmail.com>
2025-12-02 07:30:23 +0530

    Scond CHeck

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-// src/App.jsx
-import { useState } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom';
 
 import LandingPage from "./components/Landing-Page";
 import FolioMate from "./components/FolioMate";
@@ -9,26 +8,26 @@ import Contact from "./components/Contact";
 import Feedback from "./components/FeedbackForm";
 import Testimonials from "./components/Testimonials";
 import Auth from "./components/Auth";
-import Home from "./components/Home-Page";
+import Home from "./components/Home-Page"
+
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
-    <BrowserRouter>
+    <>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/folio-mate" element={<FolioMate />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/feedback" element={<Feedback />} />
-        <Route path="/testimonials" element={<Testimonials />} />
-        <Route path="/auth" element={<Auth />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/FolioMate' element={<FolioMate />} />
+        <Route path='/About' element={<About />} />
+        <Route path='/Contact' element={<Contact />} />
+        <Route path='/Feedback' element={<Feedback />} />
+        <Route path='/Testimonials' element={<Testimonials />} />
+        <Route path='/Auth' element={<Auth />} />
+        <Route path='/Home' element={<Home />} />
       </Routes>
-    </BrowserRouter>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
